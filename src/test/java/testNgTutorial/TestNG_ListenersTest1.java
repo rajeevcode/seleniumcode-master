a0c123bdfa2da6dac4f15ae0c6fd058ab602e5bf
Rajeev Kumar <rajeev.code@gmail.com>
2018-09-13 09:06:36 +0700

    CustomListener3

--- src/test/java/testNgTutorial/TestNG_ListenersTest1.java
+++ src/test/java/testNgTutorial/TestNG_ListenersTest1.java
@@ -1,34 +1,32 @@
 package testNgTutorial;
 
-import listernerPackage.CustomListener1;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
-@Listeners(CustomListener1.class)
+//@Listeners(CustomListener1.class)
 public class TestNG_ListenersTest1 {
 
   @BeforeClass
-  public void setUp(){
-    System.out.println ("Code is before class" );
+  public void setUp() {
+    System.out.println("TestNG_ListenersTest1 -> Code in before class");
   }
 
   @AfterClass
-  public void cleanUp(){
-    System.out.println ("Code is after class" );
+  public void cleanUp() {
+    System.out.println("TestNG_ListenersTest1 -> Code in after class");
   }
 
   @Test
-  public void testMethod1(){
-    System.out.println ("Code is testMethod1" );
-    Assert.assertTrue ( true );
+  public void testMethod1() {
+    System.out.println("TestNG_ListenersTest1 -> Code in testMethod1");
+    Assert.assertTrue(true);
   }
 
   @Test
-  public void testMethod2(){
-    System.out.println ("Code is testMethod2" );
-    Assert.assertTrue ( false );
+  public void testMethod2() {
+    System.out.println("TestNG_ListenersTest1 -> Code in testMethod2");
+    Assert.assertTrue(false);
   }
 }
--- src/test/java/testNgTutorial/TestNG_ListenersTest2.java
+++ src/test/java/testNgTutorial/TestNG_ListenersTest2.java
@@ -1,34 +1,32 @@
 package testNgTutorial;
 
-import listernerPackage.CustomListener2;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
-@Listeners(CustomListener2.class)
+//@Listeners(CustomListener2.class)
 public class TestNG_ListenersTest2 {
 
   @BeforeClass
   public void setUp() {
-    System.out.println("Code in before class");
+    System.out.println("TestNG_ListenersTest2 -> Code in before class");
   }
 
   @AfterClass
   public void cleanUp() {
-    System.out.println("Code in after class");
+    System.out.println("TestNG_ListenersTest2 -> Code in after class");
   }
 
   @Test
   public void testMethod1() {
-    System.out.println("Code in testMethod1");
+    System.out.println("TestNG_ListenersTest2 -> Code in testMethod1");
     Assert.assertTrue(true);
   }
 
   @Test
   public void testMethod2() {
-    System.out.println("Code in testMethod2");
+    System.out.println("TestNG_ListenersTest2 -> Code in testMethod2");
     Assert.assertTrue(false);
   }
 }
